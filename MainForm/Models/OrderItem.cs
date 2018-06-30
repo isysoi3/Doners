647840c2e7783d96aee6b159d4497ddd6c2bd95b
Ilya Sysoi <14ilya12322@gmail.com>
2018-06-30 14:53:35 +0300

    impliment order item

--- MainForm/Models/OrderItem.cs
+++ MainForm/Models/OrderItem.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//TODO: fix all namespace
 namespace UI_Example.Models
 {
     class OrderItem
     {
+        public double summary { get; private set; }
+        private List<KebabItem> kebabs;
+
+        public OrderItem()
+        {
+            summary = 0;
+            kebabs = new List<KebabItem>();
+        }
+
+        public void AddNewKebabToOrder(KebabItem item)
+        {
+            kebabs.Add(item);
+            summary += item.CountCost();
+        }
+
+        public void ClearOrder()
+        {
+            summary = 0;
+            kebabs = new List<KebabItem>();
+        }
+
     }
 }
